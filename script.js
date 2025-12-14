a6713e05681c2319f14f9b483cbf5a3f9b373659
Crixx2006 <cristhoffermanuelfa@gmail.com>
2025-12-14 21:59:13 +0100

    cambio 4

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function mensaje() {
-    alert("🎅 ¡Feliz Navidad! Que tengas un año lleno de éxitos 🎄");
+for (let i = 0; i < 50; i++) {
+  const snow = document.createElement("div");
+  snow.innerHTML = "❄";
+  snow.style.position = "fixed";
+  snow.style.left = Math.random() * 100 + "vw";
+  snow.style.top = Math.random() * -100 + "vh";
+  snow.style.fontSize = Math.random() * 20 + 10 + "px";
+  snow.style.animation = "fall " + (Math.random() * 5 + 5) + "s linear infinite";
+  document.body.appendChild(snow);
 }
